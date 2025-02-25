e35f73bafab7f56df999504723b9298eb23fc1ba
John <jonathan.plantey@gmail.com>
2025-02-25 22:10:13 +0100

    wip

--- src/Command/CollectorCommand.php
+++ src/Command/CollectorCommand.php
@@ -65,6 +65,8 @@ class CollectorCommand extends Command
 
         $metrics = array_merge($metrics, $this->context->getMetrics());
 
+        $this->logger->debug('Metrics collected', ['metrics' => $metrics]);
+
         try {
             $response = $this->client->sendMetrics($metrics);
         } catch (ClientExceptionInterface $e) {
