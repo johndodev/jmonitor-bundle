642077560f71d036ce3b2de5aeaab989050e3306
John <jonathan.plantey@gmail.com>
2025-02-27 23:11:20 +0100

    wip

--- src/JmonitorBundle.php
+++ src/JmonitorBundle.php
@@ -24,6 +24,10 @@ class JmonitorBundle extends AbstractBundle
             return;
         }
 
+        if (!$config['project_api_key']) {
+            return;
+        }
+
         $container->services()->set(Client::class)
             ->args([
                 $config['base_url'],
@@ -55,19 +59,29 @@ class JmonitorBundle extends AbstractBundle
             ])
         ;
 
-        $container->services()->set(MysqlQueriesCountCollector::class)
-            ->args([
-                service('doctrine.dbal.default_connection')
-            ])
-            ->tag('jmonitor.collector', ['name' => 'mysql.queries_count'])
-        ;
+        if ($config['collectors']['mysql']['enabled'] ?? false) {
+            $container->services()->set(MysqlQueriesCountCollector::class)
+                ->args([
+                    service('doctrine.dbal.default_connection')
+                ])
+                ->tag('jmonitor.collector', ['name' => 'mysql.queries_count'])
+            ;
 
-        $container->services()->set(MysqlSlowQueriesCollector::class)
-            ->args([
-                service('doctrine.dbal.default_connection')
-            ])
-            ->tag('jmonitor.collector', ['name' => 'mysql.slow_queries'])
-        ;
+            $container->services()->set(MysqlSlowQueriesCollector::class)
+                ->args([
+                    service('doctrine.dbal.default_connection')
+                ])
+                ->tag('jmonitor.collector', ['name' => 'mysql.slow_queries'])
+            ;
+        }
+
+        if ($config['collectors']['apache']['enabled'] ?? false) {
+
+        }
+
+        if ($config['collectors']['node']['enabled'] ?? false) {
+
+        }
     }
 
     /**
@@ -78,11 +92,46 @@ class JmonitorBundle extends AbstractBundle
         $definition->rootNode()
             ->children() // jmonitor
                 ->booleanNode('enabled')->defaultTrue()->end()
-                ->scalarNode('project_api_key')->end()
+                ->scalarNode('project_api_key')->defaultNull()->info('You can find it in your jmonitor.io settings.')->end()
                 ->scalarNode('base_url')->defaultValue('https://collector.jmonitor.io')->cannotBeEmpty()->end()
-                ->scalarNode('http_client')->end()
+                ->scalarNode('http_client')->info('Name of a Psr\Http\Client\ClientInterface service')->end()
                 ->scalarNode('cache')->cannotBeEmpty()->defaultValue('cache.app')->info('Name of a Psr\Cache\CacheItemPoolInterface service, default is "cache.app"')->end()
                 ->scalarNode('schedule')->cannotBeEmpty()->defaultValue('default')->info('Name of the schedule used to handle the recurring metrics collection, default is "default"')->end()
+                ->arrayNode('collectors')
+                    ->addDefaultsIfNotSet() // permet de récup un tableau vide si pas de config
+                    // ->useAttributeAsKey()
+                    ->children()
+                        ->arrayNode('mysql')
+                            ->children()
+                                ->booleanNode('enabled')->defaultTrue()->end()
+                                // ->scalarNode('connection')->defaultValue('doctrine.dbal.default_connection')->end()
+//                                ->booleanNode('mysql_queries_count')->defaultTrue()->end()
+//                                ->booleanNode('mysql_slow_queries')->defaultTrue()->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('apache')
+                            ->children()
+                                ->booleanNode('enabled')->defaultTrue()->end()
+                                ->scalarNode('server_status_url')->defaultValue('https://localhost/server-status')->info('Url of apache mod_status. See Google.')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('node')
+                            ->children()
+                                ->booleanNode('enabled')->defaultTrue()->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('redis')
+                            ->children()
+                                ->booleanNode('enabled')->defaultTrue()->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('php')
+                            ->children()
+                                ->booleanNode('enabled')->defaultTrue()->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
             ->end()
             ->validate()
                 ->ifTrue(fn($config) => is_array($config) && $config['enabled'] && empty($config['project_api_key']))
